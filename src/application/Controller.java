e8cfb77207dcbbb682038b6a2fec67f2ae61440a
darci <darcikhey27@gmail.com>
2016-08-20 00:21:36 -0700

    Fixed java import error with TextField

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,30 +1,41 @@
 package application;
 
-import java.awt.TextArea;
-import java.awt.TextField;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField; // use this do not use awt shit
 import javafx.scene.input.KeyEvent;
 
-public class Controller 
+public class Controller implements Initializable
 {
 	
 	// text boxes and output stuff
 	
 	//TODO: work on this thing that is giving me problems or watch youtube videos
 	
-	@FXML 
-	private TextField displayBox;
+	@FXML
+	private TextField textDisplay;
 	
 	
 	@FXML
-	public void buttonClick(KeyEvent key)
+	public void buttonClick(KeyEvent event)
 	{
-		System.out.println(key.getText());
-		System.out.println(key.getCode());
-		System.out.println(key.getCode());
+		TextField buttonText = (TextField) event.getSource();
+		textDisplay.setText(buttonText.toString());
+		
+//		System.out.println(key.getText());
+//		System.out.println(key.getCode());
+//		System.out.println(key.getCode());
+	}
+
+
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 
