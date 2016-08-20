1b925c7d7257100060fda791e49203460460efee
darci <darcikhey27@gmail.com>
2016-08-19 18:06:37 -0700

    added the buttons

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,23 +1,26 @@
 package application;
 
+import java.awt.TextArea;
 import java.awt.TextField;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyEvent;
 
 public class Controller 
 {
-	@FXML
-	private Label lblOut;
-	@FXML
-	private TextField textOut;
 	
 	@FXML
-	public void buttonClick(ActionEvent event)
+	private  TextField textOut;
+	@FXML TextArea lblOut;
+	
+	@FXML
+	public void buttonClick(KeyEvent key)
 	{
-		lblOut.setText("lblOut");
-		textOut.setText("text field");
+		System.out.println(key.getText());
+		System.out.println(key.getCode());
+		System.out.println(key.getCode());
 	}
 	
 
