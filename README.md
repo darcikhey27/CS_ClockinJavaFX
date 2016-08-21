ca69f2fe590ef1f62054f1ee4cce84a959a0a6bd
darci <darcikhey27@gmail.com>
2016-08-21 00:04:28 -0700

    Added form load Keypress event handling

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 <h2>JavaFX student/employee clock in system</h2>
 <ul>
   <li><strong>Project Name</strong> : JavaFXclockin</li>
-  <li><strong>Project description</strong> clock in system used by employees/students to </li>
+  <li><strong>Project description</strong> clock in system used by employees/students to clock in  </li>
   
   <li><strong>Used Languages</strong> : Java</li>
   
@@ -12,4 +12,4 @@
   <li><strong>Start Date</strong> : ..</li>
   
   <li><strong>Goal Finish Date</strong> : </li>
-</ul
+</ul>
--- src/application/Controller.java
+++ src/application/Controller.java
@@ -9,41 +9,125 @@ import com.sun.org.apache.bcel.internal.generic.LASTORE;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField; // use this do not use awt shit
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
 public class Controller implements Initializable
 {
-	
+
 	// text boxes and output stuff
-	
-	//TODO: work on this thing that is giving me problems or watch youtube videos
-	
+
+	// TODO: work on this thing that is giving me problems or watch youtube
+	// videos
+
 	@FXML
 	private TextField textDisplay;
-	
-	
-//	@FXML
+
+	/* main method for getting user input using mouse click */
+	@FXML
 	public void buttonClick(ActionEvent event)
 	{
-		
-			String digit = ((Button)event.getSource()).getText();
-			String b = digit;
-			b += digit;
-//			System.out.println(digit);
-//			System.out.print(digit);
-		
-			textDisplay.setText(b);
-		
-		
+		String digit = ((Button) event.getSource()).getText();
+		String old = textDisplay.getText();
+		String newText = old + digit;
+		textDisplay.setText(newText);
 	}
 
-
-	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
+	
+	/* method writing input to the textDisplay textFiled */
+	private void writeInput(int number)
+    {
+        if (textDisplay.getLength() < 15)
+        {
+        	String oldText = textDisplay.getText();
+            textDisplay.setText(oldText+number);
+        }
+        //btnEnter.Focus();
+    }// End of wrtieNumber method.
+	
+	/* getting user input from the keyboard */
+	@FXML
+	public void getKeyboardbInput(KeyEvent key)
+	{
+		KeyCode input = key.getCode();
+		// testing for DIGITS from 0 to 10
+		switch(input)
+		{
+		case DIGIT0:
+			writeInput(0);
+			break;
+		case DIGIT1:
+			writeInput(1);
+			break;
+		case DIGIT2:
+			writeInput(2);
+			break;
+		case DIGIT3:
+			writeInput(3);
+			break;
+		case DIGIT4:
+			writeInput(4);
+			break;
+		case DIGIT5:
+			writeInput(5);
+			break;
+		case DIGIT6:
+			writeInput(6);
+			break;
+		case DIGIT7:
+			writeInput(7);
+			break;
+		case DIGIT8:
+			writeInput(8);
+			break;
+		case DIGIT9:
+			writeInput(9);
+			break;
 		
+		default:
+			break;
+		}
+	}
+	
+	// standard message box to show anything
+	public void showMsgBox(String output)
+	{
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("About");
+		alert.setHeaderText(null);
+		alert.setContentText(output);
+
+		alert.showAndWait();
+		
+	}
+	@FXML
+	public void showAboutMsgbox()
+	{
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("About");
+		alert.setHeaderText(null);
+		alert.setContentText("App built by Darci K Saucedo\n"
+				+ "Computer Science Student at Eastern Washington University\n"
+				+ "Mind Wide Open!");
+
+		alert.showAndWait();
+		
+	}
+	@FXML
+	public void clearDisplayBox() // clear display box
+	{
+		textDisplay.setText("");
+	}
+	
+	public void initialize(URL arg0, ResourceBundle arg1)
+	{
+		// TODO Auto-generated method stub
+
 	}
 	
 
